d1475dde58c708bbd4debee6591067e1c56b0bc2
Zack Clements <zclement@uncc.edu>
2020-05-08 22:49:51 -0400

    Added images and champions folders

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = 'NzA1MjIxMTQ5MTkzMTQyMjcz.Xqoj0Q.7EpgINgGEnCZ6Taxwm1Li08x8cM';
 //const test = require('./Test/test.js'); UNCOMMENT WHEN ALL CONSOLE.LOG IS REMOVED FROM TEST.JS
-const champion = require('./champions/champion.json');
+//const champion = require('./champions/champion.json');
+const championName = require('./champions/championName.js');
 
 
 //League API
 let LeagueAPI = require('leagueapiwrapper');
-leagueAPIKey = "RGAPI-a74bf676-7717-4511-9787-20ea2d140d70";
+leagueAPIKey = "RGAPI-fc367ca4-0398-4ef4-8991-b72148520030";
 LeagueAPI = new LeagueAPI(leagueAPIKey, Region.NA);
 
 bot.on('ready',() =>{
     console.log('Bot is online');
 })
 bot.on('message', msg =>{
-    var content = msg.content.split(".");
+    var content = msg.content.split("! ");
     if(content[0] === "!Ornn!"){
+
+        msg.channel.send({files: ['./images/Ornn.jpg']});
         msg.channel.send("Name's Ornn. No further pleasantries needed.");
     }
     if(content[0] === "summoner"){
@@ -26,35 +29,21 @@ bot.on('message', msg =>{
                 msg.channel.send("Here is " + summonerName + "'s basic information: ")
                 msg.channel.send("Summoner Level " + accountObject['summonerLevel']);
                 LeagueAPI.getLeagueRanking(accountObject).then(function(res){
-                    var rank = new String(res[0]['tier']);
+                    var rank = String(res[0]['tier']);
                     var str = rank.toLowerCase();
                     msg.channel.send("Rank: " + str + " " + res[0]['rank']);
                     //Shoutout to Sperlmutter for helping me out!
                 })
+                return LeagueAPI.getChampionMastery(accountObject)
+                .then(function(championMasteryList)
+                    {
+                        var champId = championMasteryList[0]['championId'];
+                        msg.channel.send(championName.getChampName(champId));
+                    })
             })
+
             .catch(console.log);
-        LeagueAPI.getSummonerByName(summonerName)
-            .then(function(accountObject) {
-            // Gets match list for the account
-            return LeagueAPI.getMatchList(accountObject);
-            })
-            .then(function(activeGames) { 
-                var i;
-                for (i = 0; i < 5; i++){
-                    var matchID = activeGames['matches'][i]['gameId'];
-                    //console.log(matchID);
-                    LeagueAPI.getMatch(matchID) .then(function (res){
-                        var z;
-                        for(z = 0; z < 10; z++){
-                            if(res['participantIdentities'][z]['player']['summonerName'] == summonerName){
-                                msg.channel.send("Participant #" + (z+1) + " " + res['participantIdentities'][z]['player']['summonerName']);
-                            }
-                            //console.log("Participant #" + (z+1) + " " + res['participantIdentities'][z]['player']['summonerName']);
-                        }
-                    }) .catch(console.log);
-                }
-            })
-            .catch(console.log);
+
     }
 })
 bot.login(token);
